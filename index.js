d5f756d712d806204a51852953156cbec1353bf8
yosshinsan <yoshiari1220@gmail.com>
2020-12-05 13:52:46 +0900

    update

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ $(function(){
     let apiKey = '';
     let secretKey = '';
     let intervalProcessing;
-    let intervalMilliSecond = 5000;
+    let intervalMilliSecond = 30000;
     let pairToTrade = 'BTC/JPY'
     let pairToTradeOnExchange = 'btc_jpy'
     let sideBuy = 'buy';
     let sideSell = 'sell';
-    let addPrice = 5000;
-    let subtractPrice = 5000;
+    let addPrice = 1000;
+    let subtractPrice = 1000;
     let excludeJpyBalance = 30000;
+    let tradeAmount = 0.01; //取引量を固定する　注文が複数回に分かれて整理するためlastで正確な量が取得できないため
 
     //Startボタンクリック
     $('#yoshi_start_button').click(async() => {       
@@ -27,7 +28,95 @@ $(function(){
         ch.apiKey = apiKey;
         ch.secret = secretKey;
 
-        console.log(getDateTimeDisp());
+        let isError;
+
+        intervalProcessing = setInterval(async() => {
+            
+            console.log('開始');
+            console.log(getDateTimeDisp());
+            
+
+
+            isError = false;
+
+            //未決済注文を取得
+            //let openOrders = 
+            console.log('未決済注文の取得');
+            console.log(getDateTimeDisp());
+            await fetchOpenOrders()
+                    .then(async(openOrders) => {
+                        
+                        //未決済注文がなければ処理を行うvvvvv
+                        if(openOrders.length === 0){
+                            console.log('最後の約定注文取得');
+                            console.log(getDateTimeDisp());                            
+                            //最後に約定した注文を取得する
+                            let lastOrder = await fetchLastOrder(pairToTradeOnExchange)
+                                                    .catch((e) => {
+                                                        isError = true;
+                                                        console.log(e);});
+                            
+                            if(!isError){
+                                
+                                let lastSide = lastOrder.side;
+                            
+                                //最後の約定が買いの場合
+                                if(lastSide === sideBuy){
+                                    console.log('売り注文を入れる');
+                                    console.log(getDateTimeDisp());
+                                    
+                                    //売り注文を入れる
+                                        //最後の取得価格、取得量を取得
+                                        //最後の取得価格+円で売り注文   
+                                    let lastPrice = lastOrder.rate;
+                                    //let lastAmount = lastOrder.funds.btc;
+                                    let lastAmount = tradeAmount;
+                                    let orderPrice = Math.floor(lastPrice);
+                                    orderPrice = orderPrice + addPrice;
+                                    await createLimitSellOrder(pairToTrade,lastAmount,orderPrice).catch((e) => {console.log(e)});
+                                }
+    
+                                //最後の約定が売りの場合
+                                if(lastSide === sideSell){
+                                    console.log('買い注文を入れる');
+                                    console.log(getDateTimeDisp());                                  
+                                    
+                                    //買い注文を入れる
+                                        //現在の価格を取得
+                                        //現在の価格-円で買い注文
+                                        //注文量は日本円残高から計算
+                                    let lastPrice = await fetchLastContractPrice(pairToTrade)
+                                                            .catch((e) => {
+                                                                isError = true;
+                                                                console.log(e);});
+                                    
+                                    if(!isError){
+                                        await fetchJpyBalance()
+                                                .then(async(jpyBalance) => {
+                                                    let orderPrice = lastPrice - subtractPrice;
+                                                    jpyBalance = jpyBalance - excludeJpyBalance;
+                                                    //let orderAmount = jpyBalance / orderPrice;
+                                                    //orderAmount = Math.floor(orderAmount * 1000) / 1000;
+                                                    let orderAmount = tradeAmount;
+                                                    await createLimitBuyOrder(pairToTrade,orderAmount,orderPrice).catch((e) => {console.log(e)});
+                                                })
+                                                .catch((e) => {
+                                                    isError = true;
+                                                    console.log(e);});
+                                    }
+                                }
+                            }
+                        }
+
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    });
+                    
+                    console.log(getDateTimeDisp());
+                    console.log('終了');
+
+                }, intervalMilliSecond);
 
 
 
@@ -202,6 +291,7 @@ $(function(){
 
     $('#yoshi_stop_button').click(async() => {    
         clearInterval(intervalProcessing);
+        console.log('Stopしました。')
     });
 
 
